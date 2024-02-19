57d3a9393cb7397b0387b546ddc12c977d275f5b
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2024-02-20 01:05:08 +0500

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,53 +1,91 @@
+
+import os
 import streamlit as st
+from openai import OpenAI
+import time
+from clarifai.client.model import Model
+from clarifai.client.input import Inputs
 from clarifai.client.auth import create_stub
 from clarifai.client.auth.helper import ClarifaiAuthHelper
 from clarifai.client.user import User
 from clarifai.modules.css import ClarifaiStreamlitCSS
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
 from google.protobuf import json_format, timestamp_pb2
 
 st.set_page_config(layout="wide")
 ClarifaiStreamlitCSS.insert_default_css(st)
 
-# This must be within the display() function.
-auth = ClarifaiAuthHelper.from_streamlit(st)
-stub = create_stub(auth)
-userDataObject = auth.get_user_app_id_proto()
+st.title("Clarifai NextGen Nexus App")
 
-st.title("Simple example to list inputs")
-
-with st.form(key="data-inputs"):
-  mtotal = st.number_input(
-      "Select number of inputs to view in a table:", min_value=5, max_value=100)
-  submitted = st.form_submit_button('Submit')
-
-if submitted:
-  if mtotal is None or mtotal == 0:
-    st.warning("Number of inputs must be provided.")
-    st.stop()
-  else:
-    st.write("Number of inputs in table will be: {}".format(mtotal))
-
-  # Stream inputs from the app. list_inputs give list of dictionaries with inputs and its metadata .
-  input_obj = User(user_id=userDataObject.user_id).app(app_id=userDataObject.app_id).inputs()
-  all_inputs = input_obj.list_inputs()
-
-  #Check for no of inputs in the app and compare it with no of inputs to be displayed.
-  if len(all_inputs) < (mtotal):
-    raise Exception(
-        f"No of inputs is less than {mtotal}. Please add more inputs or reduce the inputs to be displayed !"
+message_before = {}
+def main1(client,prompt):
+    assistantId = "asst_oteb8wHPdYVFZgkAGZP6fp3l"
+    '''
+    #Creating an assistant
+        assistant = client.beta.assistants.create(
+        name="Math Tutor",
+        instructions="You are a personal math tutor. Write and run code to answer math questions.",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-3.5-turbo"
     )
+    '''
+    #Creating a Thread
+    thread = client.beta.threads.create()
+    
+    #Adding message to Thread
+    message = client.beta.threads.messages.create(
+          thread_id=thread.id,
+          role="user",
+          content= prompt
+    )
+    # Run the assistant
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistantId,
+        instructions="Please address the user as Rumaisa."
+    )
+    
+    #Run and Displaying the response
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+    while run.status != "completed":
+      time.sleep(1)
+      run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+      )
+                
+    # Retrieving the messages
+    messages = client.beta.threads.messages.list(
+      thread_id=thread.id
+    )
+    arr = {}
+    for message in reversed(messages.data):
+        message_before[message.role] = message.content[0].text.value
+        arr[message.role] = message.content[0].text.value
+    return arr
+  
+def main():
 
-  else:
-    data = []
-    #added "data_url" which gives the url of the input.
-    for inp in range(mtotal):
-      data.append({
-          "id": all_inputs[inp].id,
-          "data_url": all_inputs[inp].data.image.url,
-          "status": all_inputs[inp].status.description,
-          "created_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].created_at),
-          "modified_at": timestamp_pb2.Timestamp.ToDatetime(all_inputs[inp].modified_at),
-          "metadata": json_format.MessageToDict(all_inputs[inp].data.metadata),
-      })
-
-  st.dataframe(data)
+    Question = st.text_input("Enter a Question")
+    with st.sidebar:
+        #Clarifai credentials
+        st.subheader( "Add your OPENAI API")
+        api = st.text_input("CLARIFAI PAT " , type='password')
+    if not clarifai_pat:
+        st.warning("PLease enter api to continue")
+    else:
+        client = OpenAI(api_key=api)
+    if (st.button("Submit")) and Question:
+      with st.spinner("Wait... Generating response..."):
+        response = main1(client, Question)
+        if response:
+          for role, message in response.items():
+              with st.chat_message(role):
+                  st.markdown(message)
+if __name__ == '__main__':
+    main()
